e503e46d225bc25dfa04872e96a2c4fb346b4c2a
MuXu-0212 <109685414+MuXu-0212@users.noreply.github.com>
2023-03-03 11:21:57 +0800

    Add files via upload

---
+++ linux基础/动静态库/test.c
@@ -0,0 +1,23 @@
+#include "datastructure.h"
+
+int main(int argc, char const *argv[])
+{
+
+/**/
+	
+/*链队模块检测*/
+	// Queue* q = linkqueue_init();
+
+	// linkqueue_RandScanf( q, 50);
+	// linkqueue_print( *q);
+
+/*双向循环链表模块检测*/
+	// DNode* head = Dlinklist_create();
+ //    Dlinklist_InsertDataHead( head, 10);
+ //    Dlinklist_InsertDataHead( head, 20);
+ //    Dlinklist_InsertDataHead( head, 30);
+ //    Dlinklist_LastOrder( head);
+
+
+	return 0;
+}
