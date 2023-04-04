516f60d5974e0338456f60ca6aa61a33a492d9b9
MuXu-0212 <109685414+MuXu-0212@users.noreply.github.com>
2023-04-04 14:56:02 +0800

    Add files via upload

---
+++ linux基础/文件io/review/head.h
@@ -0,0 +1,17 @@
+#ifndef __HEAD_H_
+#define __HEAD_H_
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <sys/types.h>
+#include <dirent.h>
+#include <sys/types.h>
+#include <sys/stat.h>
+#include <fcntl.h>
+#include <unistd.h>
+#include <string.h>
+// #include <error.h>
+#include <errno.h>
+
+
+#endif 
